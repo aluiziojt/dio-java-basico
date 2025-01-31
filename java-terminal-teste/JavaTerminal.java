b397e93835a51ad8b9ed1414061d7eb904533159
Aluizio <aluizio4090@gmail.com>
2025-01-31 08:53:11 -0300

    java terminal

---
+++ java-terminal-teste/JavaTerminal.java
@@ -0,0 +1,7 @@
+package dio.java.basico.java_terminal_teste;
+
+public class JavaTerminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo terminal!");
+    }
+}
